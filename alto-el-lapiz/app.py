a5ca782fbcf2713d1435ee5f810e623b38e2c5e7
Daniel Jimenez de la Cruz <djimenez@aerotecnic.cloud>
2025-05-29 13:54:04 +0200

    nuevo

--- alto-el-lapiz/app.py
+++ alto-el-lapiz/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import random
 import string
+import os
 
 app = Flask(__name__)
 
@@ -28,4 +29,5 @@ def index():
     return render_template("index.html", letra=letra_actual, categorias=categorias)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
